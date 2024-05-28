d25747a71b4efc6137eb9a8a19ed02a83e2f6ac1
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-05-28 18:00:13 -0300

    Finalização funcionalidade Encaminhar para usuário (Demandas); Component SelectAnalistaSuporte criado; Alteração de link host api para Connection String dentro do appsettings.json

---
+++ Shared/SelectAnalistaSuporte.razor.cs
@@ -0,0 +1,99 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Shared_Static_Class.ErrorModels;
+using Shared_Static_Class.Model_DTO;
+using Shared_Static_Class.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Shared_Static_Class.Converters;
+
+
+namespace Shared_Razor_Components.Shared
+{
+    public partial class SelectAnalistaSuporte : ComponentBase
+    {
+        [Inject] public Radzen.DialogService _DialogService { get; set; }
+        [Inject] public IConfiguration _config { get; set; }
+        public HttpClient _client { get; set; } = new HttpClient();
+        private List<AnalistaOption> Analistas { get; set; } = [];
+        public bool IsBusy { get; private set; } = true;
+        public bool IsOpened { get; set; } = false;
+        public bool IsSelected => Analistas.Any(x => x.Item2 == true);
+        [Parameter] public bool IsSuporte { get; set; } = false;
+        [Parameter] public bool IsAcessoLogico { get; set; } = false;
+        [Parameter] public int SubFila { get; set; } = 0;
+        [Parameter] public AcessoModel _user { get; set; } = null;
+
+        protected class AnalistaOption
+        {
+            public AnalistaOption(ACESSOS_MOBILE_DTO item1, bool item2)
+            {
+                Item1 = item1;
+                Item2 = item2;
+            }
+
+            public ACESSOS_MOBILE_DTO Item1 { get; set; } = new();
+            public bool Item2 { get; set; } = false;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                var response = await _client.GetAsync($"{_config.GetConnectionString("api_host_link")}AnalistaSuporte/Get/IsSuporte={IsSuporte}/IsAcessoLogico={IsAcessoLogico}/SubFila={SubFila}/regional={_user.REGIONAL}/matricula={_user.MATRICULA}");
+                string responseString;
+
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    var listAnalistas = JsonConvert.DeserializeObject<IEnumerable<ACESSOS_MOBILE_DTO>>(responseString);
+                    if (listAnalistas != null)
+                    {
+                        Analistas = listAnalistas
+                            .Select(x => {return new AnalistaOption(x, false);} ).ToList();
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    //await _DialogService.Alert(string.Join(" ; ", errorResponse.Errors.Select(x => x.Value)), errorResponse.Title);
+                    //_DialogService.Close(new
+                    //{
+                    //    Analista = 0
+                    //});
+                }
+
+                IsBusy = false;
+                StateHasChanged();
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        private async void SelectAnalista()
+        {
+            _DialogService.Close(Analistas.First(x => x.Item2 == true).Item1.MATRICULA);
+        }
+
+        private void ActionSelectAnalista(AnalistaOption item) 
+        {
+            if (IsSelected)
+            {
+                Analistas.ForEach(x=> x.Item2 = false);
+            }
+
+            item.Item2 = true;
+        }
+
+    }
+}
