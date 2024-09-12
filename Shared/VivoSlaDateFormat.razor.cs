207fcd5269865e90e1d3ad44bc60b6268107d3c2
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-09-12 18:29:29 -0300

    Alteração de formato de SLA; Envio de e-mail por mudança de status; Otimização de painel de Demandas;

---
+++ Shared/VivoSlaDateFormat.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shared_Razor_Components.Shared
+{
+    public partial class VivoSlaDateFormat : ComponentBase
+    {
+        [Parameter] public int Bind { get; set; }
+        [Parameter] public bool HourFormat { get; set; }
+        [Parameter] public string Label { get; set; } = "Sla";
+
+
+        public EventCallback<int> FormatChangedEvent = new();
+        public void ValueChanged(ChangeEventArgs value)
+        {
+            int args = int.Parse(value.Value.ToString());
+
+            if (HourFormat)
+            {
+                Bind = args;
+            }
+            else
+            {
+                Bind = args * 24;
+            }
+        }
+
+        public void HouFormatChanged()
+        {
+            HourFormat = !HourFormat;
+            FormatChangedEvent.InvokeAsync(Bind);
+            StateHasChanged();
+        }
+    }
+}
