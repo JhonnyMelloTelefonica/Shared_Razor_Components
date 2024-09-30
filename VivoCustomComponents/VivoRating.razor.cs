d07512dcf0fa5b2e642e1065ad9a12fa764c47f7
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-09-30 15:55:10 -0300

    Avaliação de argumentações no Cardapio Digital

---
+++ VivoCustomComponents/VivoRating.razor.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using System.Linq.Expressions;
+
+namespace Shared_Razor_Components.VivoCustomComponents
+{
+    public partial class VivoRating : ComponentBase, IDisposable
+    {
+        [Parameter] public required string LabelText { get; set; }
+        [Parameter] public int Value { get; set; }
+        [Parameter] public string? Id { get; set; }
+        [Parameter] public string? Style { get; set; }
+        [Parameter] public bool Disable { get; set; } = false;
+        [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
+        [Parameter] public Action<int> ActionValueChanged { get; set; } = default!;
+        ElementReference[] Icons = new ElementReference[6];
+
+        void ChangeValue(int value)
+        {
+            Value = value;
+            ActionValueChanged.Invoke(Value);
+            StateHasChanged();
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
